0014bcbd9aafed78d3bdd30f5ec97dbc13133285
Kevin Buffardi <kbuffardi@csuchico.edu>
2024-11-16 22:56:00 -0800

    Add example unit test and refactored versions

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,6 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
 
 using std::string;
 
@@ -10,3 +11,36 @@ using std::string;
 bool Password::has_mixed_case(string pass){
   return false;
 }
+
+// Refactored versions:
+
+/* VERSION A
+bool Password::has_mixed_case(string str)
+{
+  int count_lower = 0;
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      count_lower++;
+    }
+  }
+  return count_lower > 0 && count_lower < str.length();
+}
+*/
+
+/* VERSION B
+bool Password::has_mixed_case(string str)
+{
+  bool found = false;
+  for(char c : str){
+    if( !found && c >= 'A' && c <= 'Z' ){
+      found = true;
+    }
+    else if( found && c >= 'a' && c <= 'z'){
+      return true;
+    }
+  }
+  return false;
+}
+*/
