de11be631a92ce8427a6e350c49afdb2539f91f3
Joe Politz <joe.politz@gmail.com>
2024-11-18 19:40:05 +0000

    test version A

--- Password.cpp
+++ Password.cpp
@@ -8,13 +8,13 @@ using std::string;
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
-  return false;
-}
+//bool Password::has_mixed_case(string pass){
+//  return false;
+//}
 
 // Refactored versions:
 
-/* VERSION A
+// VERSION A
 bool Password::has_mixed_case(string str)
 {
   int count_lower = 0;
@@ -27,7 +27,7 @@ bool Password::has_mixed_case(string str)
   }
   return count_lower > 0 && count_lower < str.length();
 }
-*/
+
 
 /* VERSION B
 bool Password::has_mixed_case(string str)
